3f9acc50801470e26e068b51e3940836be0e9130
unknown <ybouazzani@gmail.com>
2025-04-17 18:17:38 +0100

    project login page

---
+++ crm-frontend/src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
+        email,
+        password,
+      });
+
+      // Enregistre le token JWT dans localStorage
+      localStorage.setItem("token", res.data.token);
+      setMessage("Connexion réussie !");
+    // eslint-disable-next-line no-unused-vars
+    } catch (err) {
+      setMessage("Identifiants incorrects.");
+    }
+  };
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Connexion</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email :</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Mot de passe :</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Se connecter</button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  );
+}
+
+export default Login;
